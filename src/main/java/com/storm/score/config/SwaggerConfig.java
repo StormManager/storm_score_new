04d11a3d9628618d076d40af611b525e28769b14
ojy9612 <ojy9612@gmail.com>
2024-05-17 04:32:09 +0900

    swagger security 설정

--- src/main/java/com/storm/score/config/SwaggerConfig.java
+++ src/main/java/com/storm/score/config/SwaggerConfig.java
@@ -3,8 +3,12 @@ package com.storm.score.config;
  *
  */
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -21,14 +25,32 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class SwaggerConfig {
+  @Value("${jwt.header}")
+  private String header;
 
   @Bean
   public OpenAPI openAPI() {
-    return new OpenAPI()
-        .info(new Info()
+    Info info = new Info()
             .title("스톰 악보앱 API")
             .description("사용자가 함께 악보를 공유하고 등록할수 있도록 하는 서비스입니다.")
-            .version("1.0"));
-  }
+            .version("1.0");
 
+    // SecuritySecheme명
+    String jwtSchemeName = "jwtAuth";
+    // API 요청헤더에 인증정보 포함
+    SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+    // SecuritySchemes 등록
+    Components components = new Components()
+            .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                    .type(SecurityScheme.Type.APIKEY) // HTTP 방식
+                    .name(header)
+                    .in(SecurityScheme.In.HEADER)
+                    .scheme("bearer")
+                    .bearerFormat("JWT")); // 토큰 형식을 지정하는 임의의 문자(Optional)
+
+    return new OpenAPI()
+            .info(info)
+            .addSecurityItem(securityRequirement)
+            .components(components);
+  }
 }
